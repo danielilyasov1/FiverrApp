9fd1289588afec968000f32b49cc6d105d67156b
danielleilyasov <danielle.ilyas@gmail.com>
2022-07-25 12:30:38 +0300

    fix footer

--- src/cmps/app-footer.cmp.vue
+++ src/cmps/app-footer.cmp.vue
@@ -1,7 +1,5 @@
 <!-- <template>
     <footer class="footer">
         <hr>
-        Created by: <br>
-        Danielle Ilyasov, Adi Birenshtock, David Eldar <br>
-        &copy; Team D.A.D - Jul 2022
+       &copy; Created by Danielle Ilyasov & Adi Birenshtock - Jul 2022
     </footer> -->
