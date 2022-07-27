81517817fe1225cda35fbd885a167b1e97fc0de8
adi birenshtock <adibirenshtock1@gmai.com>
2022-07-27 03:25:55 +0300

    Adi fix dashboard

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,17 +1,36 @@
 <template>
 <div class="user-info-container">
+
 <section class="user-info main-layout flex" v-if="orders">
 
     <div class="user-details" >
         <img src="orders[0].buyer.imgUrl" alt="">
         <h2>{{orders[0].buyer.fullname}}</h2>
+        <hr>
+        <div class="member-Since">
+          <div class="member">
+          <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg>
+         <h6>Member since</h6>
+          </div>
+       
+         <h6>5555</h6>
+
+        </div>
 
     </div>
+<div>
 
+  
+</div>
     <div class="orders-container ">
       <div  v-for="order in orders" :key="order._id">
+      <div>
+        
+      </div>
           <div class="order">
-            <img class="gig-img" :src="order.gig.img" alt="">
+            <div class="order-info">
+
+  <img class="gig-img" :src="order.gig.img" alt="">
 
             <img class="seller-img" :src="order.seller.imgUrl" alt="">
 
@@ -28,10 +47,24 @@
               </div>
               <div>
                  <p class="title">Issued At </p>
-                <p class="info"> ${{order.gig.createdAt}}</p>
+                <p class="info"> {{order.createdAt}}</p>
               </div>
-  
+            </div>
+
+            <div class="status-container">
+              <hr>
+              <div class="status">
+
+                <h1 class="status-title">Order status:</h1>
+                <h1>{{order.status}}</h1>
+
+              </div>
+            </div>
+          
+
+        
           </div>
+            
       </div>
 
     </div>
@@ -70,6 +103,29 @@
     
     },
     methods: {
+//        showTime(time) {
+//   var date = new Date(time)
+//   date = date.toString()
+//   //'Wed Jun 01 2022 15:10:52 GMT+0300 (Israel Daylight Time)'
+//   date = date.split(" ")
+//   //(9) ['Wed', 'Jun', '01', '2022', '15:10:52', 'GMT+0300', '(Israel', 'Daylight', 'Time)']
+//   date = date.splice(0, 5)
+//   //(5) ['Wed', 'Jun', '01', '2022', '15:10:52']
+
+//   return date
+// },
+//        showTime() {
+//   var date = new Date(time)
+//   date = date.toString()
+//   //'Wed Jun 01 2022 15:10:52 GMT+0300 (Israel Daylight Time)'
+//   date = date.split(" ")
+//   //(9) ['Wed', 'Jun', '01', '2022', '15:10:52', 'GMT+0300', '(Israel', 'Daylight', 'Time)']
+//   date = date.splice(0, 5)
+//   //(5) ['Wed', 'Jun', '01', '2022', '15:10:52']
+
+//   return date
+// }
+
     
 
     },
