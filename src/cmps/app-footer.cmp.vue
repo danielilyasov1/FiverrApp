cb5fef32f179ed9fd847253db37277c0eba131fe
danielleilyasov <danielle.ilyas@gmail.com>
2022-07-30 22:52:58 +0300

    danielle feat footer

--- src/cmps/app-footer.cmp.vue
+++ src/cmps/app-footer.cmp.vue
@@ -4,3 +4,48 @@
        &copy; Created by Danielle Ilyasov & Adi Birenshtock - Jul 2022
     </footer>
 </template> -->
+<template>
+    <footer class="main-footer flex">
+        <div class="flex">
+            <div class="bottom-logo main-logo">
+                Binderr<span class="gren">.</span>
+            </div>
+            <div class="footer-text">
+                © Binderr International Ltd. 2022
+                <!-- <small>Created by Danielle Ilyasov & Adi Birenshtock</small> -->
+            </div>
+        </div>
+      <div class="social-links flex align-center">
+        <a
+        href="https://twitter.com/"
+        class="social twitter-link"
+        target="_blank"
+        >
+        </a>
+        <a
+          href="https://www.facebook.com/"
+          class="social facebook-link"
+          target="_blank"
+        >
+        </a>
+        <a
+          href="https://www.linkedin.com/"
+          class="social linkedin-link"
+          target="_blank"
+        >
+        </a>
+        <a
+          href="https://www.pinterest.com/"
+          class="social pinterest-link"
+          target="_blank"
+        >
+        </a>
+        <a
+          href="https://www.instagram.com/"
+          class="social instagram-link"
+          target="_blank"
+        >
+        </a>
+      </div>
+    </footer>
+</template>
