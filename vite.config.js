da8cb19bf7e5068e06f27328128acdf7bf748586
adi birenshtock <adibirenshtock1@gmai.com>
2022-07-31 21:37:48 +0300

    adi fix home

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    outDir: '../backend/public',
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1500
+  },
+
 })
